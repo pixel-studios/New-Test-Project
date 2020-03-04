d8491a98785334b386ec116f5d41e32b7068c15c
pixel-studios <53176816+pixel-studios@users.noreply.github.com>
2020-03-04 12:32:48 +0530

    Update constant.java
    update the java file

--- src/utility/constant.java
+++ src/utility/constant.java
@@ -2,7 +2,7 @@ package utility;
 
 public class constant {
 	public static final String URL = "https://ui.freecrm.com/";
-	 
+	 public static final String URL = "https://ui.gmail.com";
     public static final String Username = "ashwinraj2394@gmail.com";
 
     public static final String Password = "Test@123";
@@ -12,7 +12,7 @@ public class constant {
     public static final String File_TestData = "testData.xlsx";
 	public static void main(String[] args) {
 		print_r(test);
-		get_tec();
+	
 		
 
 	}
