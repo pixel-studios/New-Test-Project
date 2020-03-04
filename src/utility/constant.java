c40dce67ec69c44960e1188bbdddca0b672fce4a
Balaji <balaji@pixelstudios.local>
2020-03-04 13:22:19 +0530

    Changes

--- src/utility/constant.java
+++ src/utility/constant.java
@@ -2,11 +2,7 @@ package utility;
 
 public class constant {
 	public static final String URL = "https://ui.freecrm.com/";
-<<<<<<< HEAD
 
-=======
-	 public static final String URL = "https://ui.gmail.com";
->>>>>>> 4a3b02a6e4e4f69226979587ae090a018ff8f3a2
     public static final String Username = "ashwinraj2394@gmail.com";
 
     public static final String Password = "Test@1234";
